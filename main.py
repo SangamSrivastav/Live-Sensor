c4f11a704edbba42cc052fd03ed460709952cc97
SangamSrivastav <sangamsrivastav2002@gmail.com>
2025-04-13 19:10:15 +0530

    logger

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from sensor.exception import SensorException
+import sys
+import os
+from sensor.logger import logging
 
+def test_exception():
+    try:
+        logging.info("Divide by zero")  
+        a=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
 
 
 if __name__ == "__main__":
     try:
-        pass
+        test_exception()
     except Exception as e:
         print(e)
