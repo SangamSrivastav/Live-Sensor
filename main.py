6863764b070692138edbf5cbfcc7bfacc6be6b72
SangamSrivastav <sangamsrivastav2002@gmail.com>
2025-04-13 17:19:54 +0530

    exception

---
+++ main.py
@@ -0,0 +1,9 @@
+from sensor.exception import SensorException
+
+
+
+if __name__ == "__main__":
+    try:
+        pass
+    except Exception as e:
+        print(e)
--- setup.py
+++ setup.py
@@ -3,11 +3,31 @@
 from setuptools import setup, find_packages
 from typing import List
 
+def get_requirements() ->list[str]:
+    """
+    This function returns a list of requirements
+    """
+    # with open('requirements.txt') as f:
+    #     requirements = f.readlines()
+    
+    # # Remove any leading/trailing whitespace characters
+    # requirements = [req.strip() for req in requirements]
+    
+    # # Remove any empty strings from the list
+    # requirements = [req for req in requirements if req]
+    
+    # return requirements
+
+    requirements_list = list[str] = []
+    return requirements_list
+
+
+
 setup(
 name='sensor',
 version="0.0.1",
 author='Sangam',
 author_email="sangamsrivastav2002@gmail.com",
 packages=find_packages(),
-install_requires = ['pymongo']
+install_requires =   get_requirements() #['pymongo']
 )
